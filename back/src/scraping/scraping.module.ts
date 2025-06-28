d9b91f3f3d91cd11ffb20396f90f5ea9c9759bfe
Luis Henrique <luishenrique.gbarbosa21@gmail.com>
2025-06-28 02:39:08 -0300

    fix: import

--- back/src/scraping/scraping.module.ts
+++ back/src/scraping/scraping.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ScrapingService } from './scraping.service';
+import { AppConfigModule } from 'src/app-config/app-config.module';
 
 @Module({
 	providers: [ScrapingService],
 	exports: [ScrapingService],
+	imports: [AppConfigModule],
 })
 export class ScrapingModule {}
