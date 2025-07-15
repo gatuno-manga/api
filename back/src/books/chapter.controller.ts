1921c34aa94a93d1c1e1669169a9ff9a2942d441
Luis Henrique <luishenrique.gbarbosa21@gmail.com>
2025-07-15 07:29:23 -0300

    fix: opcional user

--- back/src/books/chapter.controller.ts
+++ back/src/books/chapter.controller.ts
@@ -3,12 +3,14 @@ import { ChapterService } from './chapter.service';
 import { JwtAuthGuard } from 'src/auth/guard/jwt-auth.guard';
 import { CurrentUserDto } from 'src/auth/dto/current-user.dto';
 import { CurrentUser } from 'src/auth/decorator/current-user.decorator';
+import { OptionalAuthGuard } from 'src/auth/guard/optional-auth.guard';
 
 @Controller('chapters')
 export class ChapterController {
     constructor(private readonly chapterService: ChapterService) {}
 
     @Get(':idChapter')
+    @UseGuards(OptionalAuthGuard)
     getChapter(
         @Param('idChapter') idChapter: string,
         @CurrentUser() user?: CurrentUserDto
