916be0a9099ae8fc2cbadfb7859667e5f644a51e
luis henrique <luishenrique.gbarbosa21@gmail.com>
2026-05-27 13:31:59 -0300

    feat(constants): remove 'description' from FULLTEXT_COLUMNS and LIKE_FALLBACK_COLUMNS

--- src/books/domain/constants/search.constants.ts
+++ src/books/domain/constants/search.constants.ts
@@ -1,8 +1,7 @@
 export const FULLTEXT_COLUMNS =
-	'`book`.`title`, `book`.`description`, `book`.`alternative_titles_text`' as const;
+	'`book`.`title`, `book`.`alternative_titles_text`' as const;
 export const LIKE_FALLBACK_COLUMNS = [
 	'book.title',
-	'book.description',
 	'book.alternative_titles_text',
 ] as const;
 export const FULLTEXT_SPECIAL_CHARS_REGEX = /[+\-><()~*"@]/g;
