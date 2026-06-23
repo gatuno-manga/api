184a85a72b418491aa87972c494103252bed74b8
jaoppb <joaopedroperes06@gmail.com>
2026-06-23 16:13:23 -0300

    fix: add missing dto decorators

--- src/book-requests/infrastructure/http/dto/book-request-response.dto.ts
+++ src/book-requests/infrastructure/http/dto/book-request-response.dto.ts
@@ -1,14 +1,34 @@
 import { BookRequestStatus } from '@/book-requests/domain/enums/book-request-status.enum';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 export class BookRequestResponseDto {
+	@ApiProperty()
 	id: string;
+
+	@ApiProperty()
 	userId: string;
+
+	@ApiProperty()
 	title: string;
+
+	@ApiProperty()
 	url: string;
+
+	@ApiPropertyOptional({ type: String, nullable: true })
 	reason: string | null;
+
+	@ApiProperty({ enum: BookRequestStatus })
 	status: BookRequestStatus;
+
+	@ApiPropertyOptional({ type: String, nullable: true })
 	adminId: string | null;
+
+	@ApiPropertyOptional({ type: String, nullable: true })
 	rejectionMessage: string | null;
+
+	@ApiProperty()
 	createdAt: Date;
+
+	@ApiProperty()
 	updatedAt: Date;
 }
